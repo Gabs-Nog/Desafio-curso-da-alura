6579689e1e86d3c71028ea917684aee5eaf7a7f2
Gabs-Nog <gabrielns.2002@hotmail.com>
2025-02-22 20:52:01 -0300

    adicionando comentarios.

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+//crie uma lista vazia.
 let listaGenerica = [ ];
+//crie uma lista com as linguagens de programação JavaScript, Python, C, C++, Kotlin.
 let linguagensDeProgramacao = [ 'JavaScript', 'Python', 'C', 'C++', 'Kotlin'];
+//adiciona as linguagens java, ruby, golang a lista.
 linguagensDeProgramacao.push ('Java', 'Ruby', 'GoLang');
+// lista de nomes com 3 nomes
 let listaNomes = [ 'Alline', 'Gabriel', 'Aminata'];
+//exibe o primeiro nome
 console.log (listaNomes [0] );
+//exibe o segundo nome
 console.log (listaNomes [1] );
+//exibe o ultimo nome
 console.log (listaNomes [listaNomes.length -1] );
