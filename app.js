e93bd8186306e8b5ddad7b6288b4c73a77220074
Gabs-Nog <gabrielns.2002@hotmail.com>
2025-02-22 17:52:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let listaGenerica = [ ];
+let linguagensDeProgramacao = [ 'JavaScript', 'Python', 'C', 'C++', 'Kotlin'];
+linguagensDeProgramacao.push ('Java', 'Ruby', 'GoLang');
+let listaNomes = [ 'Alline', 'Gabriel', 'Aminata'];
+console.log (listaNomes [0] );
+console.log (listaNomes [1] );
+console.log (listaNomes [listaNomes.length -1] );
